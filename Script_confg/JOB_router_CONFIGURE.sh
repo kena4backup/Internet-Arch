aed4420bd0bc5c5dde2072720c3662212314a6fc
Kena Teshome <Kenateshome11@gmail.com>
2025-07-11 17:29:39 +0200

    JOB configuration

--- Script_confg/JOB_router_CONFIGURE.sh
+++ Script_confg/JOB_router_CONFIGURE.sh
@@ -3,46 +3,44 @@
 conf
 ip route 6.0.0.0/8 Null0
 interface ext_8_DUS
-ip address 179.6.8.1/24
-exit
-interface host
-ip address 6.108.0.2/24
-exit
-interface lo
-ip address 6.158.0.1/24
-exit
-interface measurement_6
-ip address 6.0.199.1/24
-exit
-interface port_FRA
-ip address 6.0.11.2/24
+ ip address 179.6.8.1/24
 exit
 
-router ospf
-ospf router-id 6.158.0.1
-network 6.0.11.0/24 area 0
-network 6.0.199.0/24 area 0
-network 6.108.0.0/24 area 0
-network 6.158.0.0/24 area 0
+interface host
+ ip address 6.108.0.2/24
+exit
+
+interface lo
+ ip address 6.158.0.1/24
+exit
+
+interface measurement_6
+ ip address 6.0.199.1/24
+exit
+
+interface port_FRA
+ ip address 6.0.11.2/24
 exit
 
 router bgp 6
-bgp router-id 6.158.0.1
-neighbor 6.151.0.1 remote-as 6
-neighbor 6.151.0.1 update-source lo
-neighbor 6.152.0.1 remote-as 6
-neighbor 6.152.0.1 update-source lo
-neighbor 6.153.0.1 remote-as 6
-neighbor 6.153.0.1 update-source lo
-neighbor 6.154.0.1 remote-as 6
-neighbor 6.154.0.1 update-source lo
-neighbor 6.155.0.1 remote-as 6
-neighbor 6.155.0.1 update-source lo
-neighbor 6.156.0.1 remote-as 6
-neighbor 6.156.0.1 update-source lo
-neighbor 6.157.0.1 remote-as 6
-neighbor 6.157.0.1 update-source lo
-neighbor 179.6.8.2 remote-as 8
+ bgp router-id 6.158.0.1
+ neighbor 6.151.0.1 remote-as 6
+ neighbor 6.151.0.1 update-source lo
+ neighbor 6.152.0.1 remote-as 6
+ neighbor 6.152.0.1 update-source lo
+ neighbor 6.153.0.1 remote-as 6
+ neighbor 6.153.0.1 update-source lo
+ neighbor 6.154.0.1 remote-as 6
+ neighbor 6.154.0.1 update-source lo
+ neighbor 6.155.0.1 remote-as 6
+ neighbor 6.155.0.1 update-source lo
+ neighbor 6.156.0.1 remote-as 6
+ neighbor 6.156.0.1 update-source lo
+ neighbor 6.157.0.1 remote-as 6
+ neighbor 6.157.0.1 update-source lo
+ neighbor 179.6.8.2 remote-as 8
+ neighbor 179.6.8.2 description CUSTOMER_AS8
+ 
 
 network 6.0.0.0/8
 neighbor 6.151.0.1 next-hop-self
@@ -52,6 +50,35 @@ neighbor 6.154.0.1 next-hop-self
 neighbor 6.155.0.1 next-hop-self
 neighbor 6.156.0.1 next-hop-self
 neighbor 6.157.0.1 next-hop-self
+neighbor 179.6.8.2 route-map IN_AS8_CUSTOMER in
+neighbor 179.6.8.2 route-map OUT_AS8_CUSTOMER out
+ 
+exit
+
+router ospf
+ ospf router-id 6.158.0.1
+ network 6.0.11.0/24 area 0
+ network 6.0.199.0/24 area 0
+ network 6.108.0.0/24 area 0
+ network 6.158.0.0/24 area 0
+exit
+
+ip prefix-list ALLOWED_PREFIX_TO_CUSTOMER seq 5 permit 6.0.0.0/8
+ip prefix-list ALLOWED_PREFIX_FROM_CUSTOMER seq 5 permit 8.0.0.0/8
+
+
+bgp community-list standard ALLOWED_TAG_TO_CUSTOMER seq 5 permit 6:100 6:200 6:300
+
+route-map OUT_AS8_CUSTOMER permit 10
+match ip address prefix-list ALLOWED_PREFIX_TO_CUSTOMER
+
+route-map OUT_AS8_CUSTOMER permit 20
+match community ALLOWED_TAG_TO_CUSTOMER any
+
+route-map IN_AS8_CUSTOMER permit 10
+match ip address prefix-list ALLOWED_PREFIX_FROM_CUSTOMER
+set community 6:300
+
 
 exit
 exit
